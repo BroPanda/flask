f2b32d1b8113e1c06342822daa22a6ea947b3275
kokish <juniorik85@gmail.com>
2020-07-07 20:54:32 +0300

    2

--- app/owners/views.py
+++ app/owners/views.py
@@ -21,11 +21,15 @@ def owners_page():
     return render_template('owners/owners_page.html', owners=db, form=form)
 
 
-@owners.route('/<name>')
+@owners.route('/<name>', methods=['POST', 'GET'])
 def user_page(name):
     form = Add_Pet()
-    user = ''
+    user = None
     print(1)
+    for owner in db:
+        if owner['name'] == name:
+            user = owner
+            print(2)
     if request.method == 'POST':
         print(3)
         name_pet = request.form['name_pet']
@@ -35,9 +39,6 @@ def user_page(name):
         user['pets'].append({'name': name_pet, 'age': age_pet, 'type': type_pet})
         print(f'b', user)
 
-    for owner in db:
-        if owner['name'] == name:
-            user = owner
-            print(2)
+
 
     return render_template('owners/user_page.html', user=user, form=form)
